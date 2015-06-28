9f103ebbb086b88f6185c4f49cca4de6618d5fe3
person124 <cmacleodp@gmail.com>
2015-06-28 00:50:26 -0400

    Version: 1.10 Changes: +Added ability to have a command that reloads base and sub-plugins

--- com/person124/plugin/PersonBase.java
+++ com/person124/plugin/PersonBase.java
@@ -3,11 +3,11 @@ package com.person124.plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PersonBase extends JavaPlugin {
-	
+
 	public void onEnable() {
 		getLogger().info("has been enabled! Sub-plugins can now be used.");
 	}
-	
+
 	public void onDisable() {
 		getLogger().info("has been disabled.");
 	}
--- com/person124/plugin/PersonPlugins.java
+++ com/person124/plugin/PersonPlugins.java
@@ -2,22 +2,30 @@ package com.person124.plugin;
 
 import java.io.File;
 
+import net.md_5.bungee.api.ChatColor;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PersonPlugins extends JavaPlugin {
-	
+
 	private final boolean CONFIGURABLE;
 	private final PPBase[] SUB_PLUGINS;
 
 	private File cfgFile;
 	private FileConfiguration config;
-	
+
+	private boolean command = false;
+	private String permission;
+
 	public PersonPlugins(boolean config, PPBase... plugins) {
 		CONFIGURABLE = config;
 		SUB_PLUGINS = plugins;
 	}
-	
+
 	public void onEnable() {
 		if (CONFIGURABLE) {
 			createFolder();
@@ -38,10 +46,10 @@ public class PersonPlugins extends JavaPlugin {
 			}
 		}
 	}
-	
+
 	public void onDisable() {
 		for (PPBase ppb : SUB_PLUGINS) {
-			if (config.getBoolean(ppb.getName()))ppb.onDeactivated();
+			if (config.getBoolean(ppb.getName())) ppb.onDeactivated();
 		}
 	}
 
@@ -51,5 +59,108 @@ public class PersonPlugins extends JavaPlugin {
 			getLogger().info("Creating plugin folder....");
 		}
 	}
-	
+
+	protected void hasCommand(String perm) {
+		command = true;
+		permission = perm;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (command) {
+			if (label.equalsIgnoreCase(getName())) {
+				if (sender instanceof Player) {
+					Player p = (Player) sender;
+					if (permission != null) {
+						if (!p.hasPermission(permission)) {
+							p.sendMessage(ChatColor.RED + "You don't have permission to use this command!");
+							return true;
+						}
+					}
+
+					if (args.length == 0) getHelp(p);
+					else if (args.length == 1) {
+						if (args[0].equalsIgnoreCase("list")) listSubPlugins(p);
+						else if (args[0].equalsIgnoreCase("reload")) {
+							onDisable();
+							onEnable();
+						} else getHelp(p);
+					} else if (args.length == 2) {
+						if (args[0].equalsIgnoreCase("reload")) reloadSub(p, args[1]);
+						else getHelp(p);
+					}
+				} else {
+					if (args.length == 0) getHelp(null);
+					else if (args.length == 1) {
+						if (args[0].equalsIgnoreCase("list")) listSubPlugins(null);
+						else if (args[0].equalsIgnoreCase("reload")) {
+							onDisable();
+							onEnable();
+						} else getHelp(null);
+					} else if (args.length == 2) {
+						if (args[0].equalsIgnoreCase("reload")) reloadSub(null, args[1]);
+						else getHelp(null);
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+	private void listSubPlugins(Player p) {
+		String s = "";
+		for (PPBase plugin : SUB_PLUGINS) {
+			s += plugin.getName() + ", ";
+		}
+		if (p != null) p.sendMessage(ChatColor.GOLD + "Sub-Plugins: " + s);
+		else getLogger().info("Sub-Plugins: " + s);
+	}
+
+	private void reloadSub(Player p, String name) {
+		for (PPBase plugin : SUB_PLUGINS) {
+			if (plugin.getName().equalsIgnoreCase(name)) {
+				if (config.getBoolean(plugin.getName())) {
+					plugin.onDeactivated();
+					plugin.onActivated(this);
+					if (p != null) p.sendMessage(ChatColor.GOLD + "Sub-plugin: " + name + " has been reloaded.");
+					else getLogger().info("Sub-plugin: " + name + " has been reloaded.");
+				}
+				return;
+			}
+		}
+	}
+
+	private void getHelp(Player p) {
+		if (p != null) {
+			p.sendMessage(ChatColor.GOLD + getHelp(0));
+			p.sendMessage(ChatColor.GOLD + getHelp(1));
+			p.sendMessage(ChatColor.GOLD + getHelp(2));
+			p.sendMessage(ChatColor.GOLD + getHelp(3));
+		} else {
+			getLogger().info(getHelp(0));
+			getLogger().info(getHelp(1));
+			getLogger().info(getHelp(2));
+			getLogger().info(getHelp(3));
+		}
+	}
+
+	private String getHelp(int i) {
+		String s = "NOPE.AVI!";
+		switch (i) {
+			case 0:
+				s = "/" + getName().toLowerCase() + " | Shows this menu.";
+				break;
+			case 1:
+				s = "/" + getName().toLowerCase() + " list | Lists all sub-plugins.";
+				break;
+			case 2:
+				s = "/" + getName().toLowerCase() + " reload | Reloads all sub-plugins.";
+				break;
+			case 3:
+				s = "/" + getName().toLowerCase() + " reload <plugin name> | Reloads specified sub-plugins.";
+				break;
+		}
+		return s;
+	}
+
 }
